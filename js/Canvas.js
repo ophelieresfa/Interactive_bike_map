c924112ef5cba0e30c70335306eb1210493e9602
ophelieresfa <resfa.ophelie@yahoo.fr>
2019-07-17 18:40:08 +0200

    Add function for initTouch method

--- js/Canvas.js
+++ js/Canvas.js
@@ -42,7 +42,41 @@ class Canvas {
         }
     }
 
-    initTouch() {}
+    initTouch() {
+        let canvas = document.getElementById('sign');
+        let ctx = canvas.getContext('2d');
+
+        canvas.width = 200;
+        canvas.height = 100;
+        ctx.save();
+
+        canvas.addEventListener("touchstart", function (event) {
+            touchdown(event)
+        });
+
+        function touchdown(event) {
+            ctx.beginPath();
+            ctx.moveTo(event.offsetX, event.offsetY);
+            canvas.addEventListener("touchmove", paint);
+        }
+
+
+        function paint(event) {
+            let alertMessage = document.getElementById("alert_message");
+            alertMessage.innerText = "";
+            ctx.lineTo(event.offsetX, event.offsetY);
+            ctx.stroke();
+            sessionStorage.setItem("canvas", "1");
+        }
+
+        canvas.addEventListener("touchup", function (event) {
+            touchup(event)
+        });
+
+        function touchup() {
+            canvas.removeEventListener("touchmove", paint);
+        }
+    }
 
     clearDraw() {}
 
