3ea9db0156786f3cc687375067ee59d394678130
ophelieresfa <resfa.ophelie@yahoo.fr>
2019-07-17 18:39:37 +0200

    Add function for draw method

--- js/Canvas.js
+++ js/Canvas.js
@@ -6,7 +6,41 @@ class Canvas {
         this.reservation = 0;
     }
 
-    draw() {}
+    draw() {
+        let canvas = document.getElementById('sign');
+        let ctx = canvas.getContext('2d');
+
+        canvas.width = 200;
+        canvas.height = 100;
+        ctx.save();
+
+        canvas.addEventListener("mousedown", function (event) {
+            mousedown(event)
+        });
+
+        function mousedown(event) {
+            ctx.beginPath();
+            ctx.moveTo(event.offsetX, event.offsetY);
+            canvas.addEventListener("mousemove", paint);
+        }
+
+
+        function paint(event) {
+            let alertMessage = document.getElementById("alert_message");
+            alertMessage.innerText = "";
+            ctx.lineTo(event.offsetX, event.offsetY);
+            ctx.stroke();
+            sessionStorage.setItem("canvas", "1");
+        }
+
+        canvas.addEventListener("mouseup", function (event) {
+            mouseup(event)
+        });
+
+        function mouseup() {
+            canvas.removeEventListener("mousemove", paint);
+        }
+    }
 
     initTouch() {}
 
