680ae544978f19611db64c555b9609ea22fe8956
ophelieresfa <resfa.ophelie@yahoo.fr>
2019-07-17 18:17:18 +0200

    Add function for call Ajax

--- js/GMap.js
+++ js/GMap.js
@@ -14,5 +14,12 @@ class GoogleMap {
             center : this.amien,
             zoom: 14,
         });
+
+        ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Amiens&apiKey=5b12c800cb5e0ad63cd05a39206e90b20254719d", function (reponse) {
+            let stations = JSON.parse(reponse);
+            stations.forEach(function(station) {
+                createMarker(station);
+            });
+        });
     }
 }
