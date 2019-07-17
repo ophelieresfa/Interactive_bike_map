58b8c0bb661b5886fb6cef1d5dc7b5480491ba3a
ophelieresfa <resfa.ophelie@yahoo.fr>
2019-07-17 18:59:03 +0200

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 // Strict mode
 'use strict';
 
+// Creates the object slider
+
 var slider = new Slider();
+
+// Launch the slider
+
 slider.nextSlide();
 
+// Creates the object map
+
 var map = new GoogleMap();
+
+// Launch the map
+
 function initMap(){
     map.initMap();
 }
 
+// Creates the object location
+
 var reservation = new Reservation();
+
+// Launch the location
+
 reservation.initForm();
 reservation.keepName();
 reservation.closeForm();
 
+// Creates the object canvas
+
 var canvas = new Canvas();
+
+// Launch the canvas
+
 canvas.draw();
 canvas.clearDraw();
 canvas.initTouch();
