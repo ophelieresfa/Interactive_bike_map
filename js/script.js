9960bcb5d5035ed10cc09a417d7c86a273984440
ophelieresfa <resfa.ophelie@yahoo.fr>
2019-07-17 18:56:56 +0200

    Add lauch objects methods

--- js/script.js
+++ js/script.js
@@ -2,9 +2,22 @@
 'use strict';
 
 var slider = new Slider();
+slider.nextSlide();
 
 var map = new GoogleMap();
+function initMap(){
+    map.initMap();
+}
 
 var reservation = new Reservation();
+reservation.initForm();
+reservation.keepName();
+reservation.closeForm();
 
 var canvas = new Canvas();
+canvas.draw();
+canvas.clearDraw();
+canvas.initTouch();
+canvas.keepData();
+canvas.newReserv();
+canvas.timer();
